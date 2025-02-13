edc76a19d188974c3816618f87d5891cc9c3def2
sameersiddique987 <sameersiddique301@gmail.com>
2025-02-13 11:16:39 +0500

    Updated

--- index.js
+++ index.js
@@ -8,12 +8,29 @@ dotenv.config();
  const port = 3000;
 
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",  
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type"]
-}));
 
+// CORS Middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+// app.use(cors({
+//   origin: "http://localhost:5173",  
+//   methods: ["GET", "POST"],
+//   allowedHeaders: ["Content-Type"]
+// }));
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
