5c22cc7fd1b59551b35e647e6088cdf6a5d1a3c5
sameersiddique987 <sameersiddique301@gmail.com>
2025-02-23 12:08:03 +0500

    Updated

--- index.js
+++ index.js
@@ -24,39 +24,6 @@ const allowedOrigins = [
   'http://localhost:5173' 
 ];
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('CORS not allowed for this origin'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// }));
-
-
-// app.options('*', (req, res) => {
-//   res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.status(200).end();
-// });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// // Routes
-// app.use('/api/v1', router);
-
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
 
 // ✅ CORS Middleware
 app.use(cors({
@@ -87,6 +54,9 @@ app.options("*", (req, res) => {
   res.sendStatus(200);
 });
 
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running...");
+});
 // API Routes
 app.use("/api/v1", routes);
 
